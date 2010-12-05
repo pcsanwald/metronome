73d144b080de37110cc8af8999e543872fcd1830
Paul Sanwald <pcsanwald@gmail.com>
2010-12-05 17:13:03 -0500

    bug fixes, application preference archiving

---
+++ Classes/FileHelper.h
@@ -0,0 +1,12 @@
+/*
+ *  FileHelper.h
+ *  Metronome
+ *
+ *  Created by paul sanwald on 12/5/10.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#import <UIKit/UIKit.h>
+
+NSString *pathInDocumentDirectory(NSString *fileName);
---
+++ Classes/FileHelper.m
@@ -0,0 +1,23 @@
+/*
+ *  FileHelper.m
+ *  Metronome
+ *
+ *  Created by paul sanwald on 12/5/10.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "FileHelper.h"
+
+NSString *pathInDocumentDirectory(NSString *fileName)
+{
+	// get list of document directories in sandbox
+	NSArray *documentDirectories = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+	
+	// get one and only one document directory for that list
+	NSString *documentDirectory = [documentDirectories objectAtIndex:0];
+	
+	// append passed in filename to that directory and return it
+	return [documentDirectory stringByAppendingPathComponent:fileName];
+	
+}
